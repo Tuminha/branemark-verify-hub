8eb4702203b4e674f9e6a361dab7c576fb7b57e0
Tuminha <83960151+Tuminha@users.noreply.github.com>
2026-01-06 19:35:44 +0100

    fix: Use HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { PasswordGate } from "./components/PasswordGate";
 import { IntelligenceDashboard } from "./pages/IntelligenceDashboard";
 import Dashboard from "./pages/Dashboard";
@@ -18,14 +18,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <PasswordGate>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<IntelligenceDashboard />} />
             <Route path="/legacy" element={<Dashboard />} />
             <Route path="/validated-centers" element={<ValidatedCenters />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </PasswordGate>
     </TooltipProvider>
   </QueryClientProvider>
